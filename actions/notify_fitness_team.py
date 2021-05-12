05619e76c20856d8c8cd16b4222a1d48300991c1
Michael Butler <michaelbutler75@gmail.com>
2021-05-12 15:30:31 -0400

    updated key

--- actions/notify_fitness_team.py
+++ actions/notify_fitness_team.py
@@ -11,9 +11,9 @@ class NotifyFitness(Action):
         self.smtp_port = int(self.config['smtp_port'])
         self.smtp_user = None
         self.smtp_pass = None
-        if self.config.get('smtp_user') and self.config.get("smtp_pass"):
+        if self.config.get('smtp_user') and self.config.get("smtp_password"):
             self.smtp_user = self.config['smtp_user']
-            self.smtp_pass = self.config['smtp_pass']
+            self.smtp_pass = self.config['smtp_password']
         self.from_address = self.config['from_address']
         self.to_address = self.config['to_address']
 
@@ -33,8 +33,8 @@ class NotifyFitness(Action):
 
             print(f'Username is {self.smtp_user} and config is {self.config["smtp_user"]}')
 
-            # if self.smtp_port == 587:
-            #     smtp_server.starttls()
-            # if self.smtp_user:
-            #     smtp_server.login(user=self.smtp_user, password=self.smtp_pass)
-            # smtp_server.send_message(msg=msg)
+            if self.smtp_port == 587:
+                smtp_server.starttls()
+            if self.smtp_user:
+                smtp_server.login(user=self.smtp_user, password=self.smtp_pass)
+            smtp_server.send_message(msg=msg)
